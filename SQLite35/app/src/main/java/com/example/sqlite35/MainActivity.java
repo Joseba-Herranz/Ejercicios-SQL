c60641b5f5c6f6c7c3dda09c5a3755fe55ae648c
Joseba Andoni Herranz <josdron2@gmail.com>
2023-10-23 00:38:17 +0200

    main

--- SQLite35/app/src/main/java/com/example/sqlite35/MainActivity.java
+++ SQLite35/app/src/main/java/com/example/sqlite35/MainActivity.java
@@ -16,8 +16,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private ListView songListView;
     private List<Song> songs;
-    private Song songAdapter;
-    private DataManager databaseHelper;
+    private SongAdapter songAdapter;
+    private SongDatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +26,87 @@ public class MainActivity extends AppCompatActivity {
 
         songListView = findViewById(R.id.songListView);
         songs = new ArrayList<>();
-        songAdapter = new Song(this, songs);
+        songAdapter = new SongAdapter(this, songs); // Cambio Song a SongAdapter
         songListView.setAdapter(songAdapter);
 
-        databaseHelper = new DataManager(this);
+        databaseHelper = new SongDatabaseHelper(this);
 
-        // TODO: Implement "Add a song" feature
-        // TODO: Implement "Delete a song" feature
-        // TODO: Implement "Modify song data" feature
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = findViewById(R.id.titleEditText);
+                EditText authorEditText = findViewById(R.id.authorEditText);
+                EditText youtubeUrlEditText = findViewById(R.id.youtubeUrlEditText);
+
+                String title = titleEditText.getText().toString();
+                String author = authorEditText.getText().toString();
+                String youtubeUrl = youtubeUrlEditText.getText().toString();
+
+                Song song = new Song(title, author, youtubeUrl);
+                databaseHelper.addSong(song);
+
+                titleEditText.setText("");
+                authorEditText.setText("");
+                youtubeUrlEditText.setText("");
+
+                updateSongList(); // Refactorizar para evitar duplicación de código
+            }
+        });
+
+        Button deleteButton = findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText titleEditText = findViewById(R.id.titleEditText);
+                String title = titleEditText.getText().toString();
+                Song song = new Song(title, "", "");
+                databaseHelper.deleteSong(song);
+
+                titleEditText.setText("");
+
+                updateSongList();
+            }
+        });
+
+        Button modifyButton = findViewById(R.id.modifyButton);
+        modifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText oldTitleEditText = findViewById(R.id.oldTitleEditText);
+                EditText newTitleEditText = findViewById(R.id.newTitleEditText);
+                EditText authorEditText = findViewById(R.id.authorEditText);
+                EditText youtubeUrlEditText = findViewById(R.id.youtubeUrlEditText);
+
+                String oldTitle = oldTitleEditText.getText().toString();
+                String newTitle = newTitleEditText.getText().toString();
+                String author = authorEditText.getText().toString();
+                String youtubeUrl = youtubeUrlEditText.getText().toString();
+
+                Song oldSong = new Song(oldTitle, "", "");
+                Song newSong = new Song(newTitle, author, youtubeUrl);
+                databaseHelper.modifySong(oldSong, newSong);
+
+                oldTitleEditText.setText("");
+                newTitleEditText.setText("");
+                authorEditText.setText("");
+                youtubeUrlEditText.setText("");
+
+                updateSongList();
+            }
+        });
+    }
+
+    private void updateSongList() {
+        songs.clear();
+        songs.addAll(databaseHelper.getAllSongs());
+        songAdapter.notifyDataSetChanged();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        songs.clear();
-        songs.addAll(databaseHelper.getAllSongs());
-        songAdapter.notifyDataSetChanged();
+        updateSongList();
     }
 
     @Override
@@ -49,4 +114,4 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         databaseHelper.close();
     }
-}}
+}
